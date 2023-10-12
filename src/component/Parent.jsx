0006be60074a7bc98f9f2ed9a50ac42d8c3b8788
sujith1612 <sujithmi1612@gmail.com>
2023-10-12 14:23:49 +0530

    testing

--- src/component/Parent.jsx
+++ src/component/Parent.jsx
@@ -1,20 +1,11 @@
-import React, { useEffect, useState } from 'react'
-import Axios from 'react'
+import React  from "react";
 
 export default function Parent() {
-  const [user,setUser]=useState([])
-  useEffect(()=>{
-    Axios.get('https://jsonplaceholder.typicode.com/users').than(res=> setUser(res.data))
-  })
+
 
   return (
     <div>
-      {user.map((item)=>(
-        <li key={item.id}>{item.name}</li>
-      ))}
-      
+
     </div>
-  )
+  );
 }
-
-
